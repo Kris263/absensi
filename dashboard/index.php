cc7d9cb3c984c3dbeb892bb4e78ffa3048c664af
Kris263 <kristianptr2633@gmail.com>
2024-10-29 07:40:00 +0800

    cek

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,6 +1,15 @@
 <?php 
-    session_start();
-    echo $_SESSION['status'];
+    session_start();    
+
+    if (!isset($_SESSIOn['status']) || $_SESSION['status'] != "login") {
+        header("location:../index.php?message=Silahkan login terlebih dahulu");
+    }
+
+    if(isset($_POST['btn-logout'])) {
+        session_destroy();
+        header("location: ../index.php?message=Keluar dari sistem");
+    }
+        
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +25,14 @@
     <section>
         <h3>Halo <?php echo $_SESSION['nama_lengkap'];?></h3>
         <p>Status Pegawai: <?php echo $_SESSION['role'];?></p>
+        <br>
+        <div>
+            test
+        </div>
+        <br>
+        <form action="" method="POST">
+            <button name="btn-logout">LOGOUT</button>
+        </form>
     </section>
 </body>
 
